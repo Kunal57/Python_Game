288e6cf9bf68b976bebf512af0d9bded9a8484ce
Kunal Patel <kunalpatelx92@gmail.com>
2017-08-23 12:09:50 -0500

    Create Rectangle

--- game.py
+++ game.py
@@ -17,3 +17,16 @@ while (finished == False):
     # Looking at Event type. If type is a Quit Event then finish the game
     if event.type == pygame.QUIT:
       finished = True
+
+  # Store rectangle object in a variable
+  # Define x, y, width, and height of rectangle
+  rectOne = pygame.Rect(0,50,30,30)
+
+  # Create a tuple to hold a color
+  color = (0,0,255)
+
+  # Draws rectangle onto the screen, need to input screen, color, and rectangle object
+  pygame.draw.rect(screen, color, rectOne)
+
+  # Update the screen
+  pygame.display.flip()
